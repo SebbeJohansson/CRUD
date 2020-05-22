f2916ef7cc2f9747b2af5df3d72474c349eeb9a8
Sebastian Johansson <red@vrtxstudios.com>
2020-05-22 20:47:58 +0200

    Part 2
    Update and Delete

--- classes/db.php
+++ classes/db.php
@@ -49,6 +49,16 @@
             return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
         }
 
+        public function update($text, $id){
+            $this->query("UPDATE texts SET text = :text WHERE id = :id");
+            $this->execute(array('text' => $text, 'id' => $id));
+        }
+
+        public function delete($id){
+            $this->query("DELETE FROM texts WHERE id = :id");
+            $this->execute(array('id' => $id));
+        }
+
 
 
 
--- index.php
+++ index.php
@@ -10,14 +10,26 @@
     $db = new dbClass();
 
 
-    if(isset($_POST['text'])){
+    if(isset($_POST['id']) && isset($_POST['text'])){
+        $db->update($_POST['text'], $_POST['id']);
+
+        unset($_POST);
+        header("Location: /");
+    }elseif(isset($_POST['text'])){
         $text = $_POST['text'];
 
         $db->input($text);
 
         unset($_POST);
         header("Location: /");
+    }elseif(isset($_GET['id'])){
+        $db->delete($_GET['id']);
+
+
+        unset($_GET);
+        header("Location: /");
     }
+
 
 
 
@@ -36,10 +48,17 @@
     <input type="text" name="text"/>
     <input type="submit">
 </form>
-
+</br></br>
 <?php
 
-    var_dump($db->output());
+    $texts = $db->output();
+
+    foreach($texts as $text){
+        echo "<div> text: ".$text['text'];
+        echo "<form action='/' method='post'><input type='text' name='text' value='".$text['text']."'><input type='hidden' name='id' value='".$text['id']."'><input type='submit' value='edit'></form>";
+        echo "<a href='/?id=".$text['id']."'>delete</a>";
+        echo "</div></br>";
+    }
 
 ?>
 
